b0220f46933304663ba1d9092a2824a5625725f4
avihut <avihu.turzion@gmail.com>
2017-01-18 14:48:06 +0200

    Fixed inheritance in Ping operation class

--- py/src/operations/connectivity.py
+++ py/src/operations/connectivity.py
@@ -1,7 +1,8 @@
-from operation import Operation
+from operations.operation import Operation, OperationResult
 
 class PingOperation(Operation):
     def __init__(self, url, count=3):
+        super().__init__()
         self.url = url
         self.count = count
 
