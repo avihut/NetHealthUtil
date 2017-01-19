d2891aab62d28ef471fe478076bd3067cb503330
avihut <avihu.turzion@gmail.com>
2017-01-19 13:30:12 +0200

    Unified presenters into one package.

--- py/src/ui/term/dnsops_presenter.py
+++
@@ -1,8 +0,0 @@
-from operations import OperationDelegate
-
-class DnsLookupTerminalPresenter(OperationDelegate):
-    def operation_started(self, op):
-        print("DNS LOOKUP '%s': " % op.hostname, end='')
-
-    def operation_finished(self, op, result):
-        print("IPv4: %s, IPv6: %s" % (str(result.ipv4s), str(result.ipv6s)))
--- py/src/ui/term/presenters.py
+++ py/src/ui/term/presenters.py
@@ -1,5 +1,12 @@
-from operations import PingOpDelegate
+from operations import PingOpDelegate, OperationDelegate
 from ui.term.spinner import Spinner
+
+class DnsLookupTerminalPresenter(OperationDelegate):
+    def operation_started(self, op):
+        print("DNS LOOKUP '%s': " % op.hostname, end='')
+
+    def operation_finished(self, op, result):
+        print("IPv4: %s, IPv6: %s" % (str(result.ipv4s), str(result.ipv6s)))
 
 class PingOpTerminalPresenter(PingOpDelegate):
     def new_ping_result(self, op, ping_number, total_pings_count, ping_time):
